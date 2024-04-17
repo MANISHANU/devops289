61c489883465920896b6eb8d57bafd45fff721eb
Manisha <manishamaidam169@gmail.com.com>
2024-04-17 16:00:58 +0530

    05-var

---
+++ 05-variables .sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# size of above array is 3.
+# index are 0,1,2
+# list always starts with 0.
+
+GODS=('SHIV' 'RAM' 'KRISHN')
+
+echo "First God name is ${GODS[0]}"
+echo "Second God name is ${GODS[0]}"
+echo "Third God name is ${GODS[0]}"
+
+

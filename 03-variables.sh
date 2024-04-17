0704216b3d7853311bc0845030e77f9b0149db80
Manisha <manishamaidam169@gmail.com.com>
2024-04-17 15:33:58 +0530

    02-var

---
+++ 03-variables.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+#declaring variables
+
+P1=$1
+P2=$2
+
+#reffering variables
+
+echo "$P1 :: Hello $P2 , How are you?"
+echo "$P2 :: HI $P1, I'm fine . How are you ??"
+echo  "$P1 :: I'm fine too. Today is Rama navami .shall we go to temple??"
+echo "$P2 :: Yeah sure , Let's go "

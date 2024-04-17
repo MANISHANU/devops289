7011396ae6fc73eab32422fffbb210369516df7b
Manisha <manishamaidam169@gmail.com.com>
2024-04-17 15:29:43 +0530

    02-var

---
+++ 02-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#declaring variables
+
+P1=SITA
+P2=RAM
+
+#reffering variables
+
+echo "$P1 :: Hello $P2 , How are you?"
+echo "$P2 :: HI $P1, I'm fine . How are you ??"
+echo  "$P1 :: I'm fine too. Today is Rama navami .shall we go to temple??"
+echo "$P2 :: Yeah sure , Let's go "
+
+

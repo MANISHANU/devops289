33cda649f56d55820a1915d2bf1a98f35eda8b0b
Manisha <manishamaidam169@gmail.com.com>
2024-04-17 14:32:04 +0530

    variable

---
+++ variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ram:: Hello Sita, How are you?"
+echo "Sita:: Hi Ram,, I am fine. How are you?"
+echo "Ram: I am fine too. how is your work?"
+echo "Sita:: not bad. I am thinking to upgrade to DevOps"

0497d0bcd60145cedb183070793b54aaabf709da
Manisha <manishamaidam169@gmail.com.com>
2024-04-17 15:42:36 +0530

    04-var

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Please enter your name  ::"
-read name
+read -s name
 echo "Please enter your password ::"
-read password 
+read -s password 
 
 echo user name is $username and password is $password

4974e048accc709b3a49d4a144781154cd533a7f
Manisha <manishamaidam169@gmail.com.com>
2024-04-18 18:37:39 +0530

    color.sh

---
+++ colors.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+SCRIPT_NAME=$($0 | cut -d "." -f1)
+TIME_STAMP=$('date -%F-%H-%M-%S')
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP
+R="\e[31"
+G="\e[32"
+N="\e[0"
+
+
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+       echo -e "$2...$R Failed $N"
+    else 
+       echo -e "$2...$G Success $N"
+    fi
+}
+
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please perform this action with super user access"
+    exit 1 
+else
+   echo "You are a super user"
+fi 
+
+dnf install mysql -y &>>LOG_FILE
+VALIDTAE $? "Installing MYSQL "
+
+dnf install git -y &>>LOG_FILE
+VALIDTAE $? "Installing GIT "

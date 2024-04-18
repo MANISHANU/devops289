eea2ce855ac28543f48c60c8254254b0526bfa78
Manisha <manishamaidam169@gmail.com.com>
2024-04-18 19:02:49 +0530

    color.sh

--- colors.sh
+++ colors.sh
@@ -3,8 +3,8 @@
 
 USERID=$(id -u)
 SCRIPT_NAME=$($0 | cut -d "." -f1)
-TIME_STAMP=$('date -%F-%H-%M-%S')
-LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP
+TIME_STAMP=$('date +%F-%H-%M-%S')
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
 R="\e[31"
 G="\e[32"
 N="\e[0"

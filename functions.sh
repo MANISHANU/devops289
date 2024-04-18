2efef96096e685a7fed05e7e1de6e5851358773c
Manisha <manishamaidam169@gmail.com.com>
2024-04-18 16:27:02 +0530

    fun

--- functions.sh
+++ functions.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIME_STAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
+
 
 VALIDATE()
     {
      
-if [ $? -eq  0 ]
+if [ $? -ne  0 ]
 then 
-     echo "... Success"
+     echo "$2... Failed"
+     exit 1
 else 
    echo 
-      "  ... Failed"
-    exit 1
+      " $2... Success"
+
 fi
 
   }
@@ -26,11 +31,11 @@ else
     echo "Your are a super user"
 fi
 
-dnf install mysql -y 
+dnf install mysql -y  &>>$LOG_FILE
 
 VALIDATE $? Installing MySQL
 
-dnf install git -y
+dnf install git -y &>>$LOG_FILE
 
 VALIDATE $? Installing  Git 
 

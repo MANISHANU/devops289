48cf49d97bc15800546ce15b076df193f4925bf2
Manisha <manishamaidam169@gmail.com.com>
2024-04-18 16:32:53 +0530

    fun

--- functions.sh
+++ functions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIME_STAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
 
 
@@ -14,9 +14,7 @@ then
      echo "$2... Failed"
      exit 1
 else 
-   echo 
-      " $2... Success"
-
+   echo " $2... Success"
 fi
 
   }

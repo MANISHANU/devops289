e036b8810ddb167a392c12d300d40fbf92c29a29
Manisha <manishamaidam169@gmail.com.com>
2024-04-25 20:31:44 +0530

    calling script

---
+++ current-script.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+COURSE= "Devops"
+echo "Before calling other script :$COURSE"
+echo "process id of current script :$$"
+
+source ./other-script.sh
+
+echo "After calling other script: $COURSE "
---
+++ other-script.sh
@@ -0,0 +1,9 @@
+
+
+#!/bin/bash
+
+COURSE="DEVOPS"
+
+echo "COURSE value from other script:$COURSE"
+echo "process id from other script:$$"
+

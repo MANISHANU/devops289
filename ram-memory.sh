d2a5af70c0ac28a70b1c7b0e70f5d0c7d5f33e78
Manisha <manishamaidam169@gmail.com.com>
2024-04-27 17:15:12 +0530

    ram

---
+++ ram-memory.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+RAM_USAGE=$(free --mega -h )
+THRESHOLD=500
+
+while IFS= read -r line
+do
+    AVL=$(echo $line |  awk -F " " '{print $6F}' |cut -d "M" -f1 )
+    
+    if [ $RAM_USAGE -le $AVL ]
+    then
+        MESSAGE=" Current available ram memory is less than threshold value: $AVL \n"
+    fi
+done <<< $RAM_USAGE
+
+echo -e "Message: $MESSAGE"
+
+echo "$MESSAGE" | mail -s "RAM Usage Alert" manishamaidam169@gmail.com
+
+# echo "body" | mail -s "subject" to-address

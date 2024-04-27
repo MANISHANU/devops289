fb6b14c589a00b936205480ebc3f660bc41588c1
Manisha <manishamaidam169@gmail.com.com>
2024-04-27 15:47:32 +0530

    disk-usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -6,7 +6,7 @@ while IFS= read -r line
 do 
 USAGE=$(echo $line | awk -F " " '{print $6F}' |cut -d "%" -f1 )
 DIRECTOTY=$(echo $line |  awk -F " " '{print $NF}' )
-if [ $USAGE -ge DISK_THRESHOLD ]
+if [ $USAGE -ge $DISK_THRESHOLD ]
 then 
  echo " Disk usge is more than threshold for $DIRECTORY : CURRENT USAGE IS: $USAGE "
 fi

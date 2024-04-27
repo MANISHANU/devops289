97375c228fcb48e4b79d7d056e9b404d9c8960ee
Manisha <manishamaidam169@gmail.com.com>
2024-04-27 15:45:09 +0530

    disk-usage

---
+++ disk-usage.sh
@@ -0,0 +1,13 @@
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=30
+
+
+while IFS read -r line 
+do 
+USAGE=$(echo $line | awk -F " " '{print $6F}' |cut -d "%" -f1 )
+DIRECTOTY=$(echo $line |  awk -F " " '{print $NF}' )
+if [ $USAGE -ge DISK_THRESHOLD ]
+then 
+ echo " Disk usge is more than threshold for $DIRECTORY : CURRENT USAGE IS: $USAGE "
+fi
+done <<< DISK_USAGE
